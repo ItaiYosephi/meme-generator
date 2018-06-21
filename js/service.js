54cdad8e1a901df9103d613e610cb2b45950ce81
Itai Yosephi <itai.yosephi@gmail.com>
2018-06-21 14:57:37 +0300

    added some colors

--- js/service.js
+++ js/service.js
@@ -37,7 +37,97 @@ var gImgs = [
         id: 7,
         url: 'img/7.jpg',
         keywords: ['happy']
-    }
+    },
+    {
+        id: 8,
+        url: 'img/8.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 9,
+        url: 'img/9.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 10,
+        url: 'img/10.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 11,
+        url: 'img/11.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 12,
+        url: 'img/12.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 13,
+        url: 'img/13.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 14,
+        url: 'img/14.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 15,
+        url: 'img/15.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 16,
+        url: 'img/16.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 17,
+        url: 'img/17.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 18,
+        url: 'img/18.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 19,
+        url: 'img/19.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 20,
+        url: 'img/20.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 21,
+        url: 'img/21.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 22,
+        url: 'img/22.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 23,
+        url: 'img/23.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 24,
+        url: 'img/24.jpg',
+        keywords: ['happy']
+    },
+    {
+        id: 25,
+        url: 'img/25.jpg',
+        keywords: ['happy']
+    },
 ];
 var gCurrTxtIdx = 0;
 var gMeme = {
