73383051c8dd0206dfe3eb678726e14629cdd362
Itai Yosephi <itai.yosephi@gmail.com>
2018-06-22 17:25:58 +0300

    ss

--- js/service.js
+++ js/service.js
@@ -217,7 +217,7 @@ function addTxtLine(){
     gMeme.txts.push(makeTxt());
     moveNextTxtIdx();
     //gMeme.txts[gCurrTxtIdx].pos.x = elCanvas.width / 2;
-   // gMeme.txts[gCurrTxtIdx].pos.y = elCanvas.width / 2;
+   // gMeme.txts[gCurrTxtIdx].pos.y = elCanvas.height / 2;
     gMeme.txts[gCurrTxtIdx].pos.x = gMeme.txts[gCurrTxtIdx - 1].pos.x;
     gMeme.txts[gCurrTxtIdx].pos.y = gMeme.txts[gCurrTxtIdx - 1].pos.y +10;
     updateTxtBox();
@@ -235,7 +235,7 @@ function makeTxt() {
         weight: 400,
         pos: {
             x: elCanvas.width / 2,
-            y:  elCanvas.width / 2,
+            y:  elCanvas.height / 2,
         }
     }
 }
